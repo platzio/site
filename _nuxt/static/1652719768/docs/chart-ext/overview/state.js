92dc412b26d5200838e4f9d149a0b996376b52bc
popen2 <popen2@users.noreply.github.com>
2022-05-28 12:44:49 +0000

    deploy: 861c2e6cbb8b4197e9767b5541be85fe8c83f1c5

--- _nuxt/static/1652719768/docs/chart-ext/overview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652719768",layout:"content",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fchart-ext\u002Foverview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1a7bf60"}}}}(null));
