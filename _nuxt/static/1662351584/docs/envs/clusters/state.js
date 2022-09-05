5261c627315c6d58213d519b65d1c1fe744b8fd6
popen2 <popen2@users.noreply.github.com>
2022-09-05 04:19:49 +0000

    deploy: cf1c687eff60a8dae3f754f4e0e0c575fd9a8f9f

---
+++ _nuxt/static/1662351584/docs/envs/clusters/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662351584",layout:"content",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fenvs\u002Fclusters",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"eb7d8b40"}}}}(null));
---
+++ _nuxt/static/1662351584/docs/install/helm/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662351584",layout:"content",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Finstall\u002Fhelm",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"eb7d8b40"}}}}(null));
